96f5207cd7e52703ecdfaf561d66775227497a85
gihanrae <gihanrae@gmail.com>
2026-07-08 20:34:29 +0530

    fixed rng includes

--- src/gameLayer/randomStuff.cpp
+++ src/gameLayer/randomStuff.cpp
@@ -1,19 +1,28 @@
+
+
+
 #include "randomStuff.h"
 
-float getRandomFloat(std::ranlux24_base &rng, float min, float max)
+
+
+
+// Returns a float in [min, max)
+float getRandomFloat(std::ranlux24_base& rng, float min, float max)
 {
 	std::uniform_real_distribution<float> dist(min, max);
 	return dist(rng);
 }
 
-int getRandomInt(std::ranlux24_base &rng, int min, int max)
+// Returns an int in [min, max]
+int getRandomInt(std::ranlux24_base& rng, int min, int max)
 {
-	std::uniform_real_distribution<int> dist(min, max);
+	std::uniform_int_distribution<int> dist(min, max);
 	return dist(rng);
 }
 
-bool getRandomChance(std::ranlux24_base &rng, float chance)
+bool getRandomChance(std::ranlux24_base& rng, float chance)
 {
 	float dice = getRandomFloat(rng, 0.0, 1.0);
 	return dice <= chance;
 }
+
